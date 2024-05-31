7e68fb77924c5ce5babe98e94cedd9af453735fb
medazza <midazza27@gmail.com>
2024-05-31 19:26:19 +0100

    task 4 5 6 7 8 fix errors

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -103,9 +103,9 @@ class TestIntegrationGithubOrgClient(unittest.TestCase):
     def test_public_repos(self):
         """Tests the `public_repos` method."""
         self.assertEqual(GithubOrgClient("google").public_repos(),
-                        self.expected_repos)
+                         self.expected_repos)
 
     def test_public_repos_with_license(self):
         """Tests the `public_repos` method with a license."""
-        self.assertEqual(GithubOrgClient("google").public_repos(license="apache-2.0"),
-                         self.apache2_repos)
+        self.assertEqual(GithubOrgClient("google").public_repos(
+            license="apache-2.0"), self.apache2_repos)
